7d99235f0e0f805a8229678c7c8b1399a5f77897
Dmitry Borisov <dmi2908@yandex.ru>
2022-12-22 16:00:31 +0300

    drag and drop logic added

--- src/main/java/MouseEvents.java
+++ src/main/java/MouseEvents.java
@@ -1,45 +1,83 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.dnd.*;
 import java.awt.event.*;
 
 public class MouseEvents extends JFrame{
     static int width = 800, height = 600;
-    static int x = 0, y = 0;
+    static Point startPoint;
+    static JLayeredPane pane = new JLayeredPane();
+    static boolean needToMove=false;
     public MouseEvents() {
         super("MouseEvents");
         createGUI();
     }
 
-    public void createGUI(){
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(width, height);
-        setLayout(null);
-        setBackground(Color.white);
-        JLayeredPane pane = new JLayeredPane();
-        pane.setFocusable(true);
+    public static void start(MouseEvent e){
+        if (e.getButton() == 1) {
+            startPoint = e.getPoint();
+            needToMove =true;
+        }
+    }
+
+    public static void stop(MouseEvent e){
+        needToMove=false;
+    }
+
+    public static void move(MouseEvent e, JLabel lab){
+        if (needToMove) {
+            Point p = SwingUtilities.convertPoint(lab, e.getPoint(), pane);
+            lab.setLocation(p.x - startPoint.x, p.y - startPoint.y);
+        }
+    }
+
+    public void getMouseAdapters() {
         pane.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
                 JLabel lab = new JLabel();
                 if (e.getButton() != 3) {
                     lab.setBackground(Color.black);
-                    x = e.getX();
-                    y = e.getY();
-                    lab.setText("X: " + x + ", Y: " + y);
-                    lab.setBounds(x, y,120,20);
+                    lab.setText("X: " + e.getX() + ", Y: " + e.getY());
+                    lab.setBounds(e.getX(), e.getY(),120,20);
                     lab.setVisible(true);
                     pane.add(lab);
-                } else {
-                    pane.remove(e.getComponent().getComponentAt(e.getPoint()));
-                    pane.repaint();
+                    lab.addMouseListener(new MouseAdapter() {
+                        public void mousePressed(MouseEvent e) {
+                            start(e);
+                        }
+                        public void mouseReleased(MouseEvent e) {
+                            stop(e);
+                        }
+
+                        public void mouseClicked(MouseEvent e) {
+                            if (e.getButton() == 3) {
+                                pane.remove(e.getComponent().getComponentAt(e.getPoint()));
+                                pane.repaint();
+                            }
+                        }
+                    });
+                    lab.addMouseMotionListener(new MouseMotionAdapter() {
+                        public void mouseDragged(MouseEvent e) {
+                            move(e, lab);
+                        }
+                    });
                 }
             }
-
             @Override
             public void mouseEntered(MouseEvent e) {
                 pane.setCursor(new Cursor(Cursor.HAND_CURSOR));
             }
         });
+    }
+
+    public void createGUI() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(width, height);
+        setLayout(null);
+        setBackground(Color.white);
+        pane.setFocusable(true);
+        getMouseAdapters();
         setPreferredSize(new Dimension(width, height));
         setContentPane(pane);
     }
